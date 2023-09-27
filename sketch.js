e7c47070f92e236f4ba6e751ef09dfd97270389f
ZariaNYU <144282037+ZariaNYU@users.noreply.github.com>
2023-09-27 12:40:41 -0400

    HW03C Iteration 1
    This iteration contains a README file and completed image.

--- sketch.js
+++ sketch.js
@@ -1,6 +1,17 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(800, 800);
 }
 
 function draw() {
-}
+  background(255, 255, 255);
+  randomSeed(0);
+
+for (let I = 0; I <= 150; I = I + 1 ) {
+  x1 = random(1, 799);
+  y1 = random(1, 799);
+  x2 = random(1, 799);
+  y2 = random(1, 799);
+  fill(0);
+  line(x1, y1, x2, y2);
+  }
+} 
